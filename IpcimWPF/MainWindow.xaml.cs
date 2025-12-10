0b00d6c2e930ae9b542c3b3e31e675aed800f447
László Tamás <laszlot@kkszki.hu>
2025-12-10 11:49:31 +0100

    cs1

--- IpcimWPF/MainWindow.xaml.cs
+++ IpcimWPF/MainWindow.xaml.cs
@@ -1,24 +1,21 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using IpcimWPF;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
-namespace IpcimWPF
+namespace csudh
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    internal class Program
     {
-        public MainWindow()
+        static List<DomainEntry> entries = new List<DomainEntry>();
+
+        static void Main(string[] args)
         {
-            InitializeComponent();
+            ReadFile();
+            PrintList();
         }
+
+        
     }
 }
